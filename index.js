d1f7af8c75d9806623ded561a3648f6418515dfa
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-09-02 02:56:36 +0600

    added updat

--- index.js
+++ index.js
@@ -70,7 +70,15 @@ async function run() {
     });
 
 
-    
+    // delete a job data from db
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id:new ObjectId(id) };
+      const result = await jobsCollections.deleteOne(query)
+      res.send(result);
+
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
